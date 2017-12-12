3f7f8bbee4f947318491115a46b4100a0a946d31
Mikhail Gorbachev <mgorbachev@factset.com>
2017-12-12 23:34:23 +0000

    Added production deployment for heroku

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Run webapp
+## Run webapp
 ```bash
 npm install
 npm run dev
@@ -6,3 +6,9 @@ npm run dev
 This wil launch a front-end on http://localhost:8080
 
 This app is currently connected to the Rinkeby version of the smart contract
+
+## TODO
+* update time in details
+* add fontawesome icons
+* replace github with an icon
+* factor the timer into a component
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -52,26 +52,26 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
 module.exports = new Promise((resolve, reject) => {
   portfinder.basePort = process.env.PORT || config.dev.port
-  // portfinder.getPort((err, port) => {
-  //   if (err) {
-  //     reject(err)
-  //   } else {
-  //     // publish the new Port, necessary for e2e tests
-  //     process.env.PORT = port
-  //     // add port to devServer config
-  //     devWebpackConfig.devServer.port = port
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      // publish the new Port, necessary for e2e tests
+      process.env.PORT = port
+      // add port to devServer config
+      devWebpackConfig.devServer.port = port
 
-  //     // Add FriendlyErrorsPlugin
-  //     devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-  //       compilationSuccessInfo: {
-  //         messages: [`Your application is running here: http://${config.dev.host}:${port}`],
-  //       },
-  //       onErrors: config.dev.notifyOnErrors
-  //       ? utils.createNotifierCallback()
-  //       : undefined
-  //     }))
+      // Add FriendlyErrorsPlugin
+      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`Your application is running here: http://${config.dev.host}:${port}`],
+        },
+        onErrors: config.dev.notifyOnErrors
+        ? utils.createNotifierCallback()
+        : undefined
+      }))
 
-  //     resolve(devWebpackConfig)
-  //   }
-  // })
+      resolve(devWebpackConfig)
+    }
+  })
 })
